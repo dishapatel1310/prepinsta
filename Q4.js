532193bc2523054881871d2fecd73b5dd73bd944
dishapatel1310 <dudhatdisha7@gmail.com>
2026-02-12 10:19:23 +0530

    first commit

---
+++ Q4.js
@@ -0,0 +1,4 @@
+const n = 10;
+const sum = n * (n + 1) / 2;
+
+console.log(sum); 
---
+++ Q5.js
@@ -0,0 +1,11 @@
+function sumInRange(start, end) {
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(sumInRange(5, 10)); 
